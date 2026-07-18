ef71ceb1674d29c8f3bdd966f0e59969c77e592d
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-07-18 17:04:05 -0300

    FEAT: Início da criação e configuração do CLI

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Testandoooooo')
+import { intro,outro,select } from "@clack/prompts"
+
+intro('vue custom CLI')
+
+const opcao = await select({
+  message: 'O que deseja fazer?',
+  options: [
+    {
+      value: 'create',
+      label: 'Criar projeto'
+    },
+    {
+      value: 'component',
+      label: 'Criar componente'
+    },
+    {
+      value: 'exit',
+      label: 'Sair'
+    }
+  ]
+});
+
+console.log(opcao);
+
+outro('Até mais!');
