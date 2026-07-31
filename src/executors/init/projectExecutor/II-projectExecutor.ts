1f185e0cb86ee4405c64286b7eb4510e41f45932
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-07-31 02:00:59 -0300

    FEAT: Addition of base templates

---
+++ src/executors/init/projectExecutor/II-projectExecutor.ts
@@ -0,0 +1,142 @@
+import fs from "node:fs/promises";
+
+/*
+modul-projet/
+│
+├── public/
+│
+├── src/
+
+│   │
+│   ├── assets/
+│   │   │
+│   │   ├── images/
+│   │   ├── icons/
+│   │   ├── fonts/
+│   │   └── styles/
+│   │
+│   ├── components/
+│   │   │
+│   │   ├── ui/
+│   │   │   │
+│   │   │   └── index.ts
+│   │   │
+│   │   ├── layout/
+│   │   │   │
+│   │   │   └── index.ts
+│   │   │
+│   │   ├── navigation/
+│   │   │   │
+│   │   │   └── index.ts
+│   │   │
+│   │   ├── feedback/
+│   │   │   │
+│   │   │   └── index.ts
+│   │   │
+│   │   ├── forms/
+│   │   │   │
+│   │   │   └── index.ts
+│   │   │
+│   │   ├── data-display/
+│   │   │   │
+│   │   │   └── index.ts
+│   │   │
+│   │   ├── charts/ Somente se o chart.js for selecionado 
+│   │   │   │
+│   │   │   └── index.ts
+│   │   │
+│   │   └── index.ts
+│   │
+│   ├── views/
+│   │   │
+│   │   └── index.ts
+│   │
+│   ├── composables/
+│   │   │
+│   │   └── index.ts
+│   │
+│   ├── utils/
+│   │   │
+│   │   └── index.ts
+│   │
+│   ├── constants/
+│   │   │
+│   │   └── index.ts
+│   │
+│   ├── typescript/ Somente se o TypeScript for selecionado
+│   │   │
+│   │   ├── generics/
+│   │   │   │
+│   │   │   └── index.ts
+│   │   │
+│   │   ├── utility-Types/
+│   │   │   │
+│   │   │   └── index.ts
+│   │   │
+│   │   ├── class/
+│   │   │   │
+│   │   │   └── index.ts
+│   │   │
+│   │   ├── type-Guards/
+│   │   │   │
+│   │   │   └── index.ts
+│   │   │
+│   │   └── index.ts
+│   │
+│   ├── plugins/
+│   │   │
+│   │   └── index.ts
+│   │
+│   ├── router/
+│   │   │
+│   │   └── index.ts
+│   │
+│   ├── stores/
+│   │   │
+│   │   └── index.ts
+│   │
+│   ├── services/
+│   │   │
+│   │   └── index.ts
+│   │
+│   ├── App.vue
+│   └── main.ts
+│
+├── .gitignore
+├── README.md
+├── env.d.ts
+├── eslint.config.js
+├── index.html
+├── package-lock.json
+├── package.json
+*/
+
+
+
+
+/*
+modul-projet/
+│
+├── public/
+│
+├── src/
+│   │
+│   ├── assets/
+│   │
+│   ├── views/
+│   │
+│   ├── router/
+│   │
+│   ├── stores/
+│   │
+│   ├── App.vue
+│   └── main.ts
+│
+├── .gitignore
+├── README.md
+├── env.d.ts
+├── eslint.config.js
+├── index.html
+├── package-lock.json
+├── package.json
+*/
